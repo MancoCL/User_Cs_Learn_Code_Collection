1bcf698523acdf0837dc97d2259b203ed090fdf3
Manco Cao <MancoCL@hotmail.com>
2024-01-10 22:44:41 +0800

    Update the Product_Factroy practise.

--- Product_Factory/Program.cs
+++ Product_Factory/Program.cs
@@ -12,14 +12,16 @@ namespace Product_Factory
         {
             ProductFactory productFactory = new ProductFactory();
             WrapFactroy wrapFactroy = new WrapFactroy();
+            Logger logger = new Logger();
 
             //封装委托
             Func<Product> Milk = new Func<Product>(productFactory.MakeMilk); 
             Func<Product> Car = new Func<Product>(productFactory.MakeCar);
+            Action<Product> Log = new Action<Product>(logger.Log);
 
             //调用委托
-            Box box1 = wrapFactroy.WrapProduct(Milk);
-            Box box2 = wrapFactroy.WrapProduct(Car);
+            Box box1 = wrapFactroy.WrapProduct(Milk, Log);
+            Box box2 = wrapFactroy.WrapProduct(Car, Log);
 
             //打印输出box中的物品
             System.Console.WriteLine(box1.Product.Name.ToString());
@@ -30,28 +32,25 @@ namespace Product_Factory
         }
     }
 
-    //产品类
-    public class Product
-    {
-        public string Name { get; set; }
-    }
-    //包装盒类
-    public class Box 
-    {
-        public Product Product { get; set; }
-    }
-
     //包装厂类
     public class WrapFactroy
     { 
         //包装产品的委托方法
-        public Box WrapProduct(Func<Product> wrap_product) 
+        public Box WrapProduct(Func<Product> wrap_product,Action<Product> log) 
         {
             Box box = new Box();
+
             //获取产品
             Product product = wrap_product.Invoke();
+
+            if (product.Price > 100)
+            { 
+                log(product);
+            }
+
             //包装产品
             box.Product = product;
+
             //返回包装完成的产品
             return box;
         }
@@ -64,7 +63,8 @@ namespace Product_Factory
         {
             Product product = new Product
             {
-                Name = "Milk"
+                Name = "Milk",
+                Price = 2,
             };
             return product;
         }
@@ -73,11 +73,32 @@ namespace Product_Factory
         {
             Product product = new Product
             {
-                Name = "Car"
+                Name = "Car",
+                Price = 50000,
             };
             return product;
 
         }
 
+    }
+
+    public class Logger
+    {
+        public void Log(Product product)
+        {
+            System.Console.WriteLine("Product:'{0}',Price:'{1}',Created at '{2}'.", product.Name, product.Price, DateTime.UtcNow);
+        }
+    }
+
+    //产品类
+    public class Product
+    {
+        public string Name { get; set; }
+        public double Price { get; set; }
+    }
+    //包装盒类
+    public class Box
+    {
+        public Product Product { get; set; }
     }
 }
--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 This is my private C# learning record
-2024.01.10 Create the code library.  
-2024.01.10 Finish the product_Factroy practise.
+2024.01.10	Create the code library.  
+2024.01.10	Finish the Product_Factroy practise.
+2024.01.10	Update the Product_Factroy practise.
+				1.Add a new item named Price into the Product class.
+				2.Add a Logger class.
+				3.Add the above to the source code logic.
